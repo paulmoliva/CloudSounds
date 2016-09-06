f15e5f5f0cd683d96f4780eb964687693da4d700
paulmoliva <paulmoliva@gmail.com>
2016-09-06 10:41:34 -0700

    volume controls up and running TODO: mute css class

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -30,12 +30,11 @@ setUpPlayer(){
       }
     });
 
-    // Load in the first track
+    // Strap the player onto the window
     window.audio = a[0];
-    let first = $('ol a').attr('data-src');
-    $('ol li').first().addClass('playing');
 
-    //set upclick handlers for next and back
+
+    //set upclick handlers for next, back, and volume, playlist show/hide
     $('.skip').on('click', () => {
       var next = $('li.playing').next();
       if (!next.length) next = $('ol li').first();
@@ -49,9 +48,48 @@ setUpPlayer(){
     });
 
     $('#playlist-queue').on('click', () => {
-      console.log('click');
       $('#playlist').toggleClass('hidden');
-    })
+    });
+
+    $('.volume').hover( () => {
+      console.log('hover');
+      $('.volume-control').toggleClass('hidden');
+    });
+
+    //set volume by clicking
+    const player = $('audio')[0];
+    $('.volume-control').on('click', (e) => {
+      var offset = $('.volume-control').offset();
+      var relY = e.pageY - offset.top;
+      relY = 60 - relY;
+      player.volume = relY / 60;
+      console.log(relY);
+      $('.volume-bar').css('height', `${relY}px`);
+      $('.volume').attr('data-volume', `${relY}`);
+      $('.volume').attr('data-prevvolume', `${relY}`);
+    });
+
+    $('.volume').attr('data-volume', `60`);
+    $('.volume').attr('data-prevvolume', `60`);
+
+    $('.volume').on('click', (e) => {
+      if (e.target.className !== 'volume') return;
+      const currentVolume = parseInt($('.volume').attr('data-volume'));
+      console.log(currentVolume);
+      if (currentVolume > 0 || isNaN(currentVolume)){
+        player.volume = 0.0;
+        // $('.volume').toggleClass('muted');
+        $('.volume-bar').css('height', `0px`);
+        $('.volume').attr('data-volume', '0');
+      } else {
+        const prevvolume = parseInt($('.volume').attr('data-prevvolume'));
+        player.volume = prevvolume / 60;
+        // $('.volume').toggleClass('muted');
+        $('.volume-bar').css('height', `${prevvolume}px`);
+        $('.volume').attr('data-volume', prevvolume);
+      }
+    });
+
     // Keyboard shortcuts
     // $(document).keydown(function(e) {
     //   var unicode = e.charCode ? e.charCode : e.keyCode;
@@ -86,7 +124,7 @@ setUpPlayer(){
           <audio preload></audio>
         </div>
 
-        <div id="playlist-queue">Playlist▼</div> \
+
 
       </div>
     );
